bee720da9a8d7f159d77cb12afa85a3c3ddb8bae
xprogram24 <cexcel58@gmail.com>
2025-10-18 22:21:28 +0100

    Add view, update, and delete functionalities for doctors in management system

--- Test/Admin/DoctorManagment.py
+++ Test/Admin/DoctorManagment.py
@@ -35,21 +35,81 @@ def add_doctors():
     print(mycursor.rowcount,"record added")
 
 
+def view_doctor():
+    print("list of all Doctors Available")
+    ViewDoctor_Query = "SELECT * FROM DOCTORS"
+    mycursor.execute(ViewDoctor_Query)
+    doctor = mycursor.fetchall()
 
-
-
-
-
-
-
-
+    headers = ["fullname","specialization","contact","available_days","consultation_fee","Doctor_no"]
+    print(tabulate(doctor,headers=headers,tablefmt="fancy_grid"))
 
 
 
 
 
 def update_info():
-    print("update info")
+    view_doctor()
+    doc_no = input("Enter Doctors_NO: ").strip()
+    while True:
+        print("what would you like to change")
+        print("OPTION: ")
+        print("1. Fullname (with Dr)")
+        print("2. Specialization")
+        print("3. contact")
+        print("4. available days")
+        print("5. consultation_fee")
+        print("6. Doc_No")
+        print("7. Exit")
+
+        option = input("select an option: ")
+        if option == "1":
+            new_data = input("input new Fullname: ")
+            updateQuery = "UPDATE Doctors SET fullname = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+
+        elif option == "2":
+            new_data = input("input new specialization: ")
+            updateQuery = "UPDATE Doctors SET specialization = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+
+        elif option == "3":
+            new_data = input("input new contact: ")
+            updateQuery = "UPDATE Doctors SET contact = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+
+        elif option == "4":
+            new_data = input("input new available_days: ")
+            updateQuery = "UPDATE Doctors SET available_days = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+
+        elif option == "5":
+            new_data = input("input new consultation_fee: ")
+            updateQuery = "UPDATE Doctors SET consultation_fee = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+
+        elif option == "6":
+            new_data = input("input new doc_no: ")
+            updateQuery = "UPDATE Doctors SET doc_no = %s WHERE doc_no = %s"
+            mycursor.execute(updateQuery,(new_data,doc_no))
+            mydb.commit()
+            print("successfully updated")
+        elif option == "7":
+            break
+        else:
+            print("invalid")
+update_info()
+
 
 def view_doctor():
     print("list of all Doctors Available")
@@ -57,11 +117,41 @@ def view_doctor():
     mycursor.execute(ViewDoctor_Query)
     doctor = mycursor.fetchall()
 
-    headers = ["fullname","specialization","contact","available_days","consultation_fee"]
+    headers = ["fullname","specialization","contact","available_days","consultation_fee","Doctor_no"]
     print(tabulate(doctor,headers=headers,tablefmt="fancy_grid"))
 
 
 
 
 def delete_doctor():
-    print("delete a doctor") 
+    print("--------Delete doctors---------")
+    view_doctor()
+    doc_no = input("Enter your doctor number")
+    delete_query = "DELETE  FROM doctors WHERE doc_no = %s "
+    mycursor.execute(delete_query,(doc_no))
+    mydb.commit()
+    print("delete sucessful ✅")
+
+#fill data
+
+def add_query():
+   
+    add = "INSERT INTO Doctors (fullname, specialization, contact, available_days, consultation_fee,doc_no) VALUES (%s,%s,%s,%s,%s,%s)"
+    value = [
+         ("Dr. James Okoro", "Cardiologist", "08124567890", "Monday–Friday", "25000","CA11"),
+        ("Dr. Sarah Bello", "Dermatologist", "08135678901", "Tuesday–Saturday", "18000","DM11"),
+        ("Dr. Tunde Afolabi", "Neurologist", "08099887766", "Monday–Thursday", "30000","NE11"),
+        ("Dr. Chinyere Eze", "Pediatrician", "08123456789", "Monday–Friday", "15000","PD10"),
+        ("Dr. Kelvin Adeyemi", "Orthopedic Surgeon", "09087654321", "Wednesday–Saturday", "28000","OS21"),
+        ("Dr. Linda Ogbonna", "Gynecologist", "08034567812", "Monday–Friday", "22000","GY12"),
+        ("Dr. Yusuf Garba", "Ophthalmologist", "08198765432", "Tuesday–Friday", "20000","OP11"),
+        ("Dr. Funke Ajayi", "ENT Specialist", "09023456781", "Monday–Thursday", "17000","ENT55"),
+        ("Dr. Patrick Nwosu", "General Practitioner", "08067891234", "Monday–Sunday", "12000","GP12"),
+        ("Dr. Victoria Johnson", "Psychiatrist", "08122334455", "Wednesday–Saturday", "24000","PSY11")
+
+
+    ]
+
+    mycursor.executemany(add,value)
+    mydb.commit()
+    print("added")
